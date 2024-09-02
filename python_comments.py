b55f9f520eaa041aef05f8dee9a2aa33486e719a
QuentinCrown <113572080+QuentinCrown@users.noreply.github.com>
2024-09-02 18:38:39 -0500

    Add files via upload

---
+++ python_comments.py
@@ -0,0 +1,6 @@
+# calculating the miles per gallon of a size 15 tank
+tank_size = 15 # this value will be vertical ft
+distance = 600 # this value will be in miles
+mpg = distance / tank_size 
+# above will be 600 / 15 and the output and below will give you the answer
+print("Miles per gallon:", mpg) 
