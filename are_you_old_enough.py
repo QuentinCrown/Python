5a0013f22f8f2398f3ed39fc046bb5a517df8a7f
QuentinCrown <113572080+QuentinCrown@users.noreply.github.com>
2024-09-09 17:50:39 -0500

    Add files via upload

---
+++ are_you_old_enough.py
@@ -0,0 +1,28 @@
+# replace ("please enter your age") with your age below
+age = ( "Please enter your age"  )
+
+print(age)
+
+# driving age is 16+
+if age >= 16:
+    print("You are old enough to drive.")
+else:
+    print("You are not old enough to drive.")
+
+# voting age is 18+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
+
+# legal drinking age is 21+
+if age >= 21:
+    print("You can legally buy alcohol.")
+else:
+    print("You cannot legally buy alcohol.")
+
+# senior discount age is 65+
+if age >= 65:
+    print("You are eligible for a senior discount.")
+else:
+    print("You are not eligible for a senior discount.")
