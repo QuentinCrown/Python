3bd41b28486e190960eea78871cede08b5c5a4fe
QuentinCrown <113572080+QuentinCrown@users.noreply.github.com>
2024-09-23 14:17:16 -0500

    Add files via upload

---
+++ functions_practice.py
@@ -0,0 +1,14 @@
+# area of square
+def square(side):
+    area = side * side  
+    print(f"The area of the square is {area} square units.")
+
+# area of circle
+def circle(radius):
+    pi = 3.14  
+    area = pi * radius * radius 
+    print(f"The area of the circle is {area} square units.")
+
+# how big?
+square(16)  
+circle(9)  
